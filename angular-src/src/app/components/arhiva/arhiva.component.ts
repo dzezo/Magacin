9f7effb86cf855404ed37dad9053abc64669bd98
Ogi <ogitzar@gmail.com>
2017-09-12 23:14:53 +0200

    arhiva GET

--- angular-src/src/app/components/arhiva/arhiva.component.ts
+++ angular-src/src/app/components/arhiva/arhiva.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ItemService } from '../../services/neo4j/item.service';
+
 
 @Component({
   selector: 'app-arhiva',
@@ -8,14 +10,33 @@ import { Router } from '@angular/router';
 })
 export class ArhivaComponent implements OnInit {
 
-  constructor(private router: Router) { }
+    // User
+    user: any
+    // Arrays
+    arhArtikli: Array<any>;
+
+
+  constructor(private router: Router,
+              private itemSvc: ItemService) { }
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('user'));
+    this.getArchItems();
   }
 
   	getArtikl (artiklId){
    	localStorage.setItem('artikl',artiklId);
   	this.router.navigate(['/artikl']);
+    }
+
+    getArchItems(){
+      this.itemSvc.getArchItems(this.user.id).subscribe(archArtikli => {
+        this.arhArtikli = archArtikli;
+      }, err => {
+        console.log(err);
+        return false;
+      });
+
     }
 
 }
--- angular-src/src/app/services/neo4j/item.service.ts
+++ angular-src/src/app/services/neo4j/item.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ItemService {
 
-  constructor() { }
-
+  constructor(private http: Http) { }
+ 		
+	getArchItems(userId){
+	return this.http.get('http://localhost:7474/archive/' + userId)
+	.map(res => res.json());
+	}
 }
