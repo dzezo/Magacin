f016e5b128b211a691b8c52677486f744aa01311
Ogi <ogitzar@gmail.com>
2017-09-14 18:44:32 +0200

    Profil izlazne i ulazne fakture, logika 90% samo nisam siguran oko dobavljanja ID-a fakture da li sam to lepo odradio, i da li trebam da odem do profila artikla klikom na njegovo ime, ako trebam kako posto ne dobijam njegov ID iz GET-a?

--- angular-src/src/app/components/izlazna-faktura/izlazna-faktura.component.ts
+++ angular-src/src/app/components/izlazna-faktura/izlazna-faktura.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { InvoiceService } from '../../services/neo4j/invoice.service';
 
 @Component({
   selector: 'app-izlazna-faktura',
@@ -6,13 +9,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./izlazna-faktura.component.css']
 })
 export class IzlaznaFakturaComponent implements OnInit {
+    // Reference
+  	user: any;
+  	invoiceId: any;
+  	// Arrays
+  	Artikli = [];
+  	// Faktura
+  	invNumber: String;
+  	purchaser: String;
+  	issueDate: String;
+  	total: String;
 
-		invNumber: String = "invNumberPH";
-		total: String = "sumNumberPH";
+  	constructor(private router: Router,
+              private flashMessage: FlashMessagesService,
+              private invoiceSvc: InvoiceService) { }
 
-  constructor() { }
+  	ngOnInit() {
+  		this.user = JSON.parse(localStorage.getItem('user'));
+      this.invoiceId = localStorage.getItem('faktura'); //      ??????? DA li je ovo tacno ???????
+  	}
 
-  ngOnInit() {
-  }
-
+    getFakturu(invoiceId){
+      this.invoiceSvc.getOutputInvoice(invoiceId).subscribe(reply => {
+          if(reply.success){
+            var invoice = reply.invoice;
+            // Detalji
+            this.purchaser = invoice.purchaser.toString();
+            this.invNumber = invoice.invNumber.toString();
+            this.issueDate = invoice.issueDate.toString();
+            this.total = invoice.total.toString();
+            // Artikli
+            var newElement;
+            invoice.items.forEach((input)=>{
+              newElement = {
+                name: invoice.items.name,
+                quantity: invoice.items.quantity,
+                code: invoice.items.code,
+                sellingP: invoice.items.sellingP
+              };
+              this.Artikli.push(newElement);
+            });
+          }
+          else{
+            this.flashMessage.show(reply.msg, {cssClass: 'alert-danger', timeout: 3000});
+            return false;
+          }
+        }, err => {
+          console.log(err);
+          return false;
+        });
+    }
 }
--- angular-src/src/app/components/ulazna-faktura/ulazna-faktura.component.ts
+++ angular-src/src/app/components/ulazna-faktura/ulazna-faktura.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { InvoiceService } from '../../services/neo4j/invoice.service';
 
 @Component({
   selector: 'app-ulazna-faktura',
@@ -6,13 +9,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ulazna-faktura.component.css']
 })
 export class UlaznaFakturaComponent implements OnInit {
+	// Reference
+ 	user: any;
+ 	invoiceId: any;
+ 	// Arrays
+ 	Artikli = [];
+ 	// Faktura
+ 	refNumber: String;
+ 	invNumber: String;
+ 	taxId: String;
+ 	suplier: String;
+ 	recvDate: String;
+ 	expDate: String;
+ 	total: String;
 
-	invNumber: String = "invNumberPH";
-	total: String = "sumNumberPH";
+	constructor(private router: Router,
+	            private flashMessage: FlashMessagesService,
+	            private invoiceSvc: InvoiceService) { }
 
-  constructor() { }
+	ngOnInit() {
+		this.user = JSON.parse(localStorage.getItem('user'));
+		this.invoiceId = localStorage.getItem('faktura'); //			??????? DA li je ovo tacno ???????
+		this.getFakturu(this.invoiceId)
+	}
 
-  ngOnInit() {
-  }
-
+	getFakturu(invoiceId){
+		this.invoiceSvc.getInputInvoice(invoiceId).subscribe(reply => {
+	      if(reply.success){
+	      	var invoice = reply.invoice;
+	      	// Detalji
+	      	this.refNumber = invoice.refNumber.toString();
+	      	this.invNumber = invoice.invNumber.toString();
+	      	this.taxId = invoice.taxId.toString();
+	      	this.suplier = invoice.suplier.toString();
+	      	this.recvDate = invoice.recvDate.toString();
+	      	this.expDate = invoice.expDate.toString();
+	      	this.total = invoice.total.toString();
+	      	// Artikli
+	      	var newElement;
+	      	invoice.items.forEach((input)=>{
+	      		newElement = {
+	      			name: invoice.items.name,
+	      			quantity: invoice.items.quantity,
+	      			code: invoice.items.code,
+	      			purchaseP: invoice.items.purchaseP,
+	      			sellingP: invoice.items.sellingP
+	      		};
+	      		this.Artikli.push(newElement);
+	      	});
+	      }
+	      else{
+	      	this.flashMessage.show(reply.msg, {cssClass: 'alert-danger', timeout: 3000});
+	      	return false;
+	      }
+	    }, err => {
+	      console.log(err);
+	      return false;
+	    });
+	}
 }
